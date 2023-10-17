7e1b7f75bb0ef93529a2df776c5db6aafe4d8621
MatiasRamirezCano <matramirezca@unal.edu.co>
2023-10-17 11:35:54 -0500

    a

---
+++ futbolista.py
@@ -0,0 +1,34 @@
+from persona import Persona
+from deportista import Deportista
+
+class Futbolista(Persona, Deportista):
+    listaFutbolistas = []
+    def __init__(self, nombre, edad, altura, sexo, deporte, añosPracticando, golesMarcados, tarjetasRojas, piernaHabil):
+        Persona.__init__(self, nombre, edad, altura, sexo)
+        Deportista.__init__(self, deporte, añosPracticando)
+        self._golesMarcados = golesMarcados
+        self._tarjetasRojas = tarjetasRojas
+        self._piernaHabil = piernaHabil
+        Futbolista.listaFutbolistas.append(self)
+
+    def getGolesMarcados(self):
+        return self._golesMarcados
+    def setGolesMarcados(self, golesMarcados):
+        self._golesMarcados = golesMarcados
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+    def setTarjetasRojas(self, tarjetasRojas):
+        self._tarjetasRojas = tarjetasRojas
+    def getPiernaHabil(self):
+        return self._piernaHabil
+    def setPiernaHabil(self, piernaHabil):
+        self._piernaHabil = piernaHabil
+
+    def __str__(self):
+        cadena = f"Mi nombre es [self.getNombre] soy profesional en el deporte [self.getDeporte] Tengo [self.getEdad] años de edad y llevo [self.getAñosPracticando] años en el deporte"
+
+    
+
+    
+
+
